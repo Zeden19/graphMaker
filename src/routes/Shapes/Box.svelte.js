93b70f81ad153ac4a774fd008e18154a5189d6a6
arjunsharma <ashar562@uwo.ca>
2026-01-04 13:05:30 -0500

    Refac: Remove rects from shapes

--- src/routes/Shapes/Box.svelte.js
+++ src/routes/Shapes/Box.svelte.js
@@ -4,37 +4,6 @@ import rotateCords from "$lib/rotateCords.js";
 export class Box extends BasicShape {
   constructor(offset, getShapeArray, canvasScale, properties = {}) {
     super(offset, getShapeArray, canvasScale, properties);
-
-    this.rect = $derived({
-      top: {
-        ...rotateCords(this.position.x + (this.widthWithScale / 2), this.position.y - this.strokeWidth, this.center, this.rotation)
-      },
-
-      left: {
-        ...rotateCords(this.position.x - this.strokeWidth, this.position.y + (this.heightWithScale / 2), this.center, this.rotation)
-      },
-
-      right: {
-        ...rotateCords(this.position.x + this.widthWithScale + this.strokeWidth, this.position.y + (this.heightWithScale / 2), this.center, this.rotation)
-      },
-
-      bottom: {
-        ...rotateCords(this.position.x + (this.widthWithScale / 2), this.position.y + this.heightWithScale + this.strokeWidth, this.center, this.rotation)
-      },
-
-      topLeft: {
-        ...rotateCords(this.position.x - this.strokeWidth, this.position.y - this.strokeWidth, this.center, this.rotation),
-      },
-      topRight: {
-        ...rotateCords(this.position.x + this.widthWithScale + this.strokeWidth, this.position.y - this.strokeWidth, this.center, this.rotation),
-      },
-      bottomLeft: {
-        ...rotateCords(this.position.x - this.strokeWidth, this.position.y + this.heightWithScale + this.strokeWidth, this.center, this.rotation),
-      },
-      bottomRight: {
-        ...rotateCords(this.position.x + this.widthWithScale + this.strokeWidth, this.position.y + this.heightWithScale + this.strokeWidth, this.center, this.rotation)
-      },
-    });
   }
 
 }
--- src/routes/Shapes/Circle/circle.svelte.js
+++ src/routes/Shapes/Circle/circle.svelte.js
@@ -1,64 +1,8 @@
 import {BasicShape} from "../shape.svelte.js";
-import rotateCords from "$lib/rotateCords.js";
-
-const bottomRightAngle = 45 * Math.PI / 180; // 45 degrees
-const bottomLeftAngle = 135 * Math.PI / 180; // 135 degrees
-const topRightAngle = 315 * Math.PI / 180; // 315 degrees
-const topLeftAngle = 225 * Math.PI / 180; // 225 degrees
 
 export class Circle extends BasicShape {
   constructor(offset, getShapeArray, canvasScale, properties = {}) {
     super(offset, getShapeArray, canvasScale, properties);
-    // halving strokeWidth because stroke is drawn from center of line, rather than outside
-    this.rect = $derived(
-      {
-        top: {
-          ...rotateCords(this.position.x, this.position.y - this.heightWithScale - this.strokeWidth / 2, {
-            x: this.position.x,
-            y: this.position.y
-          }, this.rotation)
-        },
-        bottom: {
-          ...rotateCords(this.position.x, this.position.y + this.heightWithScale + this.strokeWidth / 2, {
-            x: this.position.x,
-            y: this.position.y
-          }, this.rotation)
-        },
-        left: {
-          ...rotateCords(this.position.x - this.widthWithScale - this.strokeWidth / 2, this.position.y, {
-            x: this.position.x,
-            y: this.position.y
-          }, this.rotation)
-        },
-        right: {
-          ...rotateCords(this.position.x + this.widthWithScale + this.strokeWidth / 2, this.position.y, {
-            x: this.position.x,
-            y: this.position.y
-          }, this.rotation)
-        },
-
-        bottomRight: {
-          ...this.#createCornerPos(bottomRightAngle)
-        },
-        bottomLeft: {
-          ...this.#createCornerPos(bottomLeftAngle)
-        },
-        topLeft: {
-          ...this.#createCornerPos(topLeftAngle)
-        },
-        topRight: {
-          ...this.#createCornerPos(topRightAngle)
-        },
-      }
-    );
-  }
-
-  #createCornerPos(angle) {
-    return {
-      ...rotateCords(this.position.x + (this.widthWithScale + this.strokeWidth / 2) * Math.cos(angle),
-        this.position.y + (this.heightWithScale + this.strokeWidth / 2) * Math.sin(angle),
-        {x: this.position.x, y: this.position.y}, this.rotation),
-    };
   }
 }
 
