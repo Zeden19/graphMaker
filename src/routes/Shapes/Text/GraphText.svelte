43ba3c0d93fc6b749c18da72d7c365482c0db20e
arjunsharma <ashar562@uwo.ca>
2026-01-15 16:25:40 -0500

    Refac: GraphText is a rect now transparent rect so that the stroke style are consistent

--- src/routes/Shapes/Text/GraphText.svelte
+++ src/routes/Shapes/Text/GraphText.svelte
@@ -1,14 +1,28 @@
 <script>
-  import ShapeText from "./ShapeText.svelte";
+  import {scale} from "svelte/transition";
+  import ShapeText from "../Text/ShapeText.svelte";
 
-  let {text = $bindable()} = $props();
+  let {text} = $props();
 
-  // Use axis-aligned top-left for rendering, just like Square
+  // Use the axis-aligned top-left for rendering
   let renderPosition = $derived(text.getAxisAlignedTopLeft());
+  let strokeInset = $derived(text.strokeWidth / 2);
 </script>
 
-<ShapeText x={renderPosition.x} y={renderPosition.y}
-           width={text.width} height={text.height} shape={text}
+<rect
+  transition:scale|global={{duration: 120}}
+  style="transform-origin: {text.center.x}px {text.center.y}px;
+         rotate: {text.rotation}deg;"
+  stroke="{text.strokeColor}"
+  stroke-width="{text.strokeWidth}"
+  x="{renderPosition.x - strokeInset}"
+  y="{renderPosition.y - strokeInset}"
+  width="{text.width + text.strokeWidth}"
+  height="{text.height + text.strokeWidth}"
+  fill="none"
+  bind:this={text.ref}
+></rect>
+
+<ShapeText shape={text} x={renderPosition.x} y={renderPosition.y}
            transformOrigin={text.center}
-           style="outline: {text.strokeWidth}px {text.strokeColor} solid;"
-/>
+           width={text.width} height={text.height}></ShapeText>
