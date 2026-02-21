316dbbab099212d4f9dfa7e660377ca78aa33308
arjunsharma <ashar562@uwo.ca>
2026-02-21 12:02:00 -0500

    Fix: tests using wrong db url (they cannot access docker). Used 2 different urls and dynamically assigned connectionString depending if we are in test or not.

--- backend/src/stores/db.ts
+++ backend/src/stores/db.ts
@@ -1,7 +1,10 @@
 import {Pool} from 'pg';
-
 require('dotenv').config({quiet: true});
 
+const isTest = process.env.NODE_ENV === "test";
+
+const connectionString = isTest ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL;
+
 export const db = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString,
 });
